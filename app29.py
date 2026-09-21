9f81420b0d072b6ccc8a6b31dec2aa8d960015a5
Sanjitha <ssanjitha3@gmail.com>
2026-09-21 11:37:08 +0530

    app29.py

--- app29.py
+++ app29.py
@@ -1,21 +1,18 @@
-#list of list, list of tuple, list of dict example
-# A list of lists is a list where each element is itself a list. This allows for the creation of multi-dimensional data structures, such as matrices or grids.
-# Example:
+#list of list
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]   
 print(matrix)
-# A list of tuples is a list where each element is a tuple. Tuples are immutable sequences, and using them in a list allows for the storage of fixed-size collections of items.
-# Example:
+# A list of tuples 
 matrix = [(
     (1, 2, 3),
     (4, 5, 6),
     (7, 8, 9)
 )]
 print(matrix)
-# example list of dictionary
+# list of dictionary
 matrix = [(
     {'n1':'1'},
     {'n2':'4'},
