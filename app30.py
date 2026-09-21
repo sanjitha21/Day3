94e43887290231078040f676267d4b20f824ea3f
Sanjitha <ssanjitha3@gmail.com>
2026-09-21 11:38:28 +0530

    app30.py

--- app30.py
+++ app30.py
@@ -1,6 +1,4 @@
-# explain tuple of list with example
-# explain tuple of list wu
-
+#tuple of list
 my_tuple = ([1, 2], [3, 4], [5, 6])
 
 print("Tuple of lists:")
@@ -25,8 +23,6 @@ print("\nSecond item of first tuple:")
 print(my_tuple[0][1])
 
 #tuple of dict
-# explain tuple of dict with example
-
 my_tuple = (
     {"name": "Alice", "age": 20},
     {"name": "Bob", "age": 25},
